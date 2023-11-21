66624eea0d0302409f5c0e97b1449d542a923c10
Paulo Arthur <pauloarthurcostamendes@gmail.com>
2023-11-21 10:12:43 +0000

    fix: adjusting navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,13 +1,13 @@
 <script>
-  import logo from './assets/logo.svg'
+  import logo from '../assets/logo.png'
 </script>
 
 <!-- Component: Basic Navbar -->
 <header class="relative z-20 w-full border-b shadow-lg border-b-1 border-slate-200 bg-[#4e4e4e] shadow-slate-700/5 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">
   <div class="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]">
-    <nav aria-label="main navigation" class="flex h-[5.5rem] items-stretch justify-between font-medium text-slate-700" role="navigation">
+    <nav aria-label="main navigation" class="flex h-[5.5rem] items-stretch justify-between lg:justify-start font-medium text-slate-700" role="navigation">
       <!-- Brand logo -->
-      <a id="WindUI" aria-label="WindUI logo" aria-current="page" class="flex items-center gap-2 py-3 text-lg whitespace-nowrap focus:outline-none lg:flex-1" href="javascript:void(0)">
+      <a id="WindUI" aria-label="WindUI logo" aria-current="page" class="flex items-center gap-2 py-3 pr-3 text-lg whitespace-nowrap focus:outline-none" href="javascript:void(0)">
         <img src={logo} class="logo" alt="Demo Logo" />
       </a>
       <!-- Mobile trigger -->
@@ -19,27 +19,27 @@
         </div>
       </button>
       <!-- Navigation links -->
-      <ul role="menubar" aria-label="Select page" class="invisible absolute top-0 left-0 z-[-1] ml-auto h-screen w-full justify-center overflow-hidden overflow-y-auto overscroll-contain bg-[#4e4e4e] px-8 pb-12 pt-28 font-medium opacity-0 transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0 lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0 lg:pt-0 lg:opacity-100">
+      <ul role="menubar" aria-label="Select page" class="invisible absolute top-0 left-0 z-[-1] h-screen w-full justify-center overflow-hidden overflow-y-auto overscroll-contain bg-[#4e4e4e] px-8 pb-12 pt-28 font-medium opacity-0 transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0 lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0 lg:pt-0 lg:opacity-100">
         <li role="none" class="flex items-stretch">
-          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span></a>
+          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 text-white transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span></a>
         </li>
         <li role="none" class="flex items-stretch">
-          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
+          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 text-white transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
         </li>
         <li role="none" class="flex items-stretch">
-          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
+          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 text-white transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
         </li>
         <li role="none" class="flex items-stretch">
-          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
+          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 text-white transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
         </li>
         <li role="none" class="flex items-stretch">
-          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
+          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 text-white transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
         </li>
         <li role="none" class="flex items-stretch">
-          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
+          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 text-white transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
         </li>
         <li role="none" class="flex items-stretch">
-          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
+          <a role="menuitem" aria-haspopup="false" class="flex items-center gap-2 py-4 text-white transition-colors duration-300 focus:outline-none focus-visible:outline-none lg:px-8" href="javascript:void(0)"> <span>Menu link</span> </a>
         </li>
       </ul>
     </nav>
@@ -51,10 +51,5 @@
   .logo {
     height: 6em;
     padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
   }
 </style>
