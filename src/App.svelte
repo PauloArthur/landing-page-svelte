6848126604291dc898d73de41e8cbf0710694b50
Paulo Arthur <pauloarthurcostamendes@gmail.com>
2023-11-21 11:16:28 +0000

    feat: MainContent Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
   import Navbar from './lib/Navbar.svelte'
   import Hero from './lib/Hero.svelte'
+  import MainContent from './lib/MainContent.svelte'
 </script>
 
 <main>
   <Navbar />
   <Hero />
+  <MainContent />
 </main>
 
 <style>
---
+++ src/lib/MainContent.svelte
@@ -0,0 +1,35 @@
+<script>
+  import contentImage from '../assets/content_image.jpg'
+  import Section from './Section.svelte'
+  const content = {
+    title: 'Content Main Title',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi dolor, tincidunt id mauris non, tristique aliquet magna. Morbi consectetur eu ipsum ut vestibulum. Ut tristique lobortis lectus id lacinia. Sed hendrerit dictum purus, vel porttitor sem. Cras lacus magna, fermentum in pulvinar sed, pellentesque id magna. \n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi dolor, tincidunt id mauris non, tristique aliquet magna. Morbi consectetur eu ipsum ut vestibulum. Ut tristique lobortis lectus id lacinia. Sed hendrerit dictum purus, vel porttitor sem. Cras lacus magna, fermentum in pulvinar sed, pellentesque id magna.\n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi dolor, tincidunt id mauris non, tristique aliquet magna. Morbi consectetur eu ipsum ut vestibulum. Ut tristique lobortis lectus id lacinia. Sed hendrerit dictum purus, vel porttitor sem. Cras lacus magna, fermentum in pulvinar sed, pellentesque id magna.\n\n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nisi dolor, tincidunt id mauris non, tristique aliquet magna. Morbi consectetur eu ipsum ut vestibulum. Ut tristique lobortis lectus id lacinia. Sed hendrerit dictum purus, vel porttitor sem. Cras lacus magna, fermentum in pulvinar sed, pellentesque id magna.'.replaceAll('\n', '<br>'),
+    image: contentImage,
+  }
+</script>
+
+<!-- Component: Basic Hero Mock -->
+<Section containerClasses="py-16 2xl:max-w-7xl">
+  <h1 class="text-center uppercase text-2xl font-bold mb-8">{content.title}</h1>
+  <div class="flex flex-row">
+    <div>
+      <img src={content.image} alt="" class="rounded border-2 border-black border-solid lg:mr-16">
+    </div>
+    <p>{@html content.text}</p>
+  </div>
+</Section>
+<!-- End Basic Hero Mock-->
+
+<style>
+  h1, p {
+    color: theme('colors.neutral.900');
+  }
+  h1::after {
+    content: "";
+    height: 4px;
+    width: 60px;
+    display: block;
+    background: black;
+    margin: 10px auto;
+  }
+</style>
