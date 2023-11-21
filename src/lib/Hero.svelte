bd5de399e2b9d06d5f3761695beb7e1c56aa6dfe
Paulo Arthur <pauloarthurcostamendes@gmail.com>
2023-11-21 20:07:58 +0000

    feat: successfully lazyloading slider

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,19 +1,21 @@
 <script>
-  import slide1 from '../assets/swiper_slide_1.jpg';
-  import slide2 from '../assets/swiper_slide_2.jpg';
+  import slide1 from '../assets/swiper_slide_1.webp';
+  import slide2 from '../assets/swiper_slide_2.webp';
   import Carousel from './components/Carousel.svelte';
   import Section from './components/Section.svelte';
 
   let images = [slide1]
 
-  const load = () => {
+  const loadImages = () => {
     images = [slide1, slide2]
   }
 </script>
 
 <!-- Component: Basic Hero Mock -->
 <Section containerClasses="2xl:px-0 2xl:max-w-[120rem]">
-  <Carousel images={images} on:moveSlide={load}/>
+  <Carousel let:item items={images} on:loadSlider={loadImages}>
+    <img alt="" src={item} class="w-full max-w-full max-h-full m-auto" />
+  </Carousel>
 </Section>
 <!-- End Basic Hero Mock-->
 
