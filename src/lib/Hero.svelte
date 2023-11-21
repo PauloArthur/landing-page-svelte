9ed85781403858a6cf8d34f18baf3301ce32af27
Paulo Arthur <pauloarthurcostamendes@gmail.com>
2023-11-21 16:43:21 +0000

    fix: import and using second slide image

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,11 +1,19 @@
 <script>
-  import slide1 from '../assets/swiper_slide_1.jpg'
-  import Section from './components/Section.svelte'
+  import slide1 from '../assets/swiper_slide_1.jpg';
+  import slide2 from '../assets/swiper_slide_2.jpg';
+  import Carousel from './components/Carousel.svelte';
+  import Section from './components/Section.svelte';
+
+  let images = [slide1]
+
+  const load = () => {
+    images = [slide1, slide2]
+  }
 </script>
 
 <!-- Component: Basic Hero Mock -->
 <Section containerClasses="2xl:px-0 2xl:max-w-[120rem]">
-  <img src={slide1} alt="" class="">
+  <Carousel images={images} on:moveSlide={load}/>
 </Section>
 <!-- End Basic Hero Mock-->
 
