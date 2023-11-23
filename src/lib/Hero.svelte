7d65aafd10accffb0b17fc030e957fcbbb68d751
Paulo Arthur <pauloarthurcostamendes@gmail.com>
2023-11-23 13:23:13 +0000

    minor carousel adjustments

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <!-- Component: Basic Hero Carousel -->
-<Section sectionClasses="bg-neutral-900 bg-opacity-20" containerClasses="px-0 max-w-[120rem]">
+<Section sectionClasses="bg-neutral-900 bg-opacity-20" containerClasses="!px-0 max-w-[120rem]">
   <Carousel let:item let:key items={images} carouselName='hero-glider' on:loadSlider={loadImages}>
     <img
       alt=""
--- src/lib/components/Carousel.svelte
+++ src/lib/components/Carousel.svelte
@@ -24,7 +24,9 @@
       autoplay: false,
       animationDuration: 700,
       classes: {
-        activeNav: '[&>*]:bg-slate-700',
+        nav: {
+          active: 'dot-active',
+        }
       },
       ...options,
     });
@@ -62,17 +64,17 @@
   </div>
   <!-- Controls -->
   {#if showNavigation}
-    <div class="absolute left-0 flex items-center justify-between w-full h-0 px-4 top-1/2 " data-glide-el="controls">
-      <button on:click={triggerLoad} class="inline-flex items-center justify-center w-8 h-8 transition duration-300 border rounded-full lg:w-12 lg:h-12 text-slate-700 border-slate-700 hover:text-slate-900 hover:border-slate-900 focus-visible:outline-none bg-white/20" data-glide-dir="<" aria-label="prev slide">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+    <div class="absolute left-0 flex items-center justify-between w-full h-0 px-2 lg:px-4 top-1/2" data-glide-el="controls">
+      <button on:click={triggerLoad} class="inline-flex items-center justify-center w-10 h-10 lg:w-20 lg:h-20" data-glide-dir="<" aria-label="prev slide">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke="currentColor" width="80px" height="80px" viewBox="0 0 1024 1024">
           <title>prev slide</title>
-          <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+          <path d="M768 903.232l-50.432 56.768L256 512l461.568-448 50.432 56.768L364.928 512z"/>
         </svg>
       </button>
-      <button on:click={triggerLoad} class="inline-flex items-center justify-center w-8 h-8 transition duration-300 border rounded-full lg:w-12 lg:h-12 text-slate-700 border-slate-700 hover:text-slate-900 hover:border-slate-900 focus-visible:outline-none bg-white/20" data-glide-dir=">" aria-label="next slide">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+      <button on:click={triggerLoad} class="inline-flex items-center justify-center w-10 h-10 lg:w-20 lg:h-20" data-glide-dir=">" aria-label="next slide">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" stroke="currentColor" width="80px" height="80px" viewBox="0 0 1024 1024">
           <title>next slide</title>
-          <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+          <path d="M256 120.768L306.432 64 768 512l-461.568 448L256 903.232 659.072 512z"/>
         </svg>
       </button>
     </div>
@@ -82,9 +84,15 @@
     <div class={`${indicatorClasses} flex items-center justify-center w-full gap-2`} data-glide-el="controls[nav]">
       {#each items as _, key}
         <button class="p-4 group" data-glide-dir={`=${key}`} aria-label={`goto slide ${key + 1}`}>
-          <span class="block w-2 h-2 transition-colors duration-300 rounded-full ring-1 ring-slate-700 bg-white/20 focus:outline-none" />
+          <span class="block w-6 h-6 transition-colors duration-300 bg-white rounded-full ring-1 ring-slate-700 focus:outline-none" />
         </button>
       {/each}
     </div>
   {/if}
 </div>
+
+<style>
+  .dot-active span {
+    background-color: theme('colors.slate.500/20') !important;
+  }
+</style>
